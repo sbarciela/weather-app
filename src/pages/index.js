ec8d32bd1f6be9bd80daf58c3eae9b600c3c2044
sebastian barciela <barcielasebastian@gmail.com>
2023-10-24 09:25:45 -0300

    editando readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -81,7 +81,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Weather report</title>
+        <title>Weather Report</title>
         <meta name="description" content="A weather aplication for the devchallenges.io proposal" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
